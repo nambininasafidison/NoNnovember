997fb3859b2d455cc876023930796159343314c2
nambininasafidison <safidison23@gmail.com>
2024-11-09 23:29:38 +0300

    Configuration of the server api

--- Client/src/utils/serverConfig.ts
+++ Client/src/utils/serverConfig.ts
@@ -1,5 +1,1 @@
-const serverProduction = import.meta.env.VITE_API_URL;
-const serverDevelopment = `http://${import.meta.env.VITE_SERVER_URL}:${
-  import.meta.env.VITE_SERVER_PORT
-}`;
-export const API = serverProduction || serverDevelopment;
+export const API = import.meta.env.VITE_API_URL;
